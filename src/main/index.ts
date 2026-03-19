1172e18b5784b5dd992159263dfcc1a3db4caf49
Teng Yeng Ming <jasmineteng04@gmail.com>
2026-03-19 18:19:55 +0800

    updated index.ts to refine dialog message

--- src/main/index.ts
+++ src/main/index.ts
@@ -42,7 +42,7 @@ function setupAutoUpdater(): void {
     dialog
       .showMessageBox({
         type: 'info',
-        title: 'Update Available 🎉',
+        title: 'Update Available',
         message: `Streakly ${info.version} is ready!`,
         detail: 'Download and install the latest version now?',
         buttons: ['Download and Install', 'Later'],
